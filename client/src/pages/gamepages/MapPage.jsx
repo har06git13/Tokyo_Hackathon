933231fc9999028601b95590c484e95313f43281
wina-wich <ionio.create@gmail.com>
2025-08-08 15:55:45 +0900

    MapPageを簡単に実装

--- client/src/pages/gamepages/MapPage.jsx
+++ client/src/pages/gamepages/MapPage.jsx
@@ -1,10 +1,34 @@
-import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Flex } from "@chakra-ui/react";
+import { Header } from "../../components/common";
+import { Footer } from "../../components/game-page";
+import { MapMarkerLegend, MapSpotInfo } from "../../components/game-page";
 
 export const MapPage = () => {
+  const [spotSelected, setSpotSelected] = useState(false);
+  useEffect(() => {
+    setSpotSelected(false);
+  }, []);
+
   return (
-    <div className="page-container">
-      <Text>This is MapPage</Text>
-    </div>
+    <Flex
+      className="page-container"
+      backgroundColor={"var(--color-base12)"}
+      zIndex={0}
+      position={"relative"}
+    >
+      <Header prevPage={false} currentPage="マップ" />
+      <Flex
+        className="page-contents"
+        flex={1}
+        backgroundColor={"var(--color-theme11)"}
+        justifyContent={"space-between"}
+        onClick={() => setSpotSelected((prev) => !prev)}
+      >
+        <MapMarkerLegend />
+        {spotSelected && <MapSpotInfo type="map" />}
+      </Flex>
+      <Footer type="map" />
+    </Flex>
   );
 };
