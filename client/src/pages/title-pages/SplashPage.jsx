5a4552569488ee8c5fd0438c10859177f123fc81
wina-wich <ionio.create@gmail.com>
2025-08-09 21:49:54 +0900

    splashpageを実装

--- client/src/pages/title-pages/SplashPage.jsx
+++ client/src/pages/title-pages/SplashPage.jsx
@@ -1,98 +1,37 @@
-import React, { useState } from "react";
-import { Flex, Text } from "@chakra-ui/react";
-import {
-  Header,
-  Button,
-  LifeGauge,
-  LifeGaugeElement,
-  Toggle,
-} from "../../components/common";
-import { TitleInputField, CheckBox } from "../../components/title-page";
-import {
-  SettingElement,
-  SettingInfoIndex,
-  LifeGaugeListElement,
-} from "../../components/game-page";
-import {
-  MonologueLine,
-  LogElement,
-  MapMarkerLegend,
-  MapSpotInfo,
-  EventText,
-  ActionTab,
-  ActionConfirmDialog,
-  CheckinButton,
-  TweetCard,
-  Footer,
-} from "../../components/game-page";
+import React, { useEffect } from "react";
+import { Flex, Image } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const MotionImage = motion(Image);
 
 export const SplashPage = () => {
-  const [selectedValue, setSelectedValue] = useState("");
+  const navigate = useNavigate();
 
-  const sampleOptions = [
-    { value: "apple", label: "りんご" },
-    { value: "banana", label: "バナナ" },
-    { value: "orange", label: "オレンジ" },
-  ];
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/title/gamestart");
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <Flex
       className="page-container"
-      width={"100vw"}
-      minHeight={"100vh"}
-      flexDirection={"column"}
+      backgroundColor={"var(--color-theme10)"}
       justifyContent={"center"}
-      backgroundColor={"blue"}
+      paddingBottom={"40vw"}
     >
-      <Header />
-      <Flex
-        className="page-contents"
-        flex={1}
-        flexDirection={"column"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Footer />
-        <TweetCard />
-        <CheckinButton />
-        <ActionConfirmDialog />
-        <ActionTab />
-        <EventText />
-        <MapSpotInfo life={+10} mental={-10} charge={0} money={0} />
-        <MapMarkerLegend />
-        <LogElement
-          eventtype="event"
-          spottype="battery"
-          text="モバイルバッテリーをレンタルした。"
-        />
-        <Text>This is SplashPage</Text>
-        <Button
-          text="Start"
-          isAvairable
-          color="var(--color-theme10)"
-          width="40%"
-        />
-        <LifeGaugeElement />
-        <LifeGauge />
-        <Toggle />
-        <TitleInputField
-          isSelect={true}
-          value={selectedValue}
-          onChange={(e) => setSelectedValue(e.target.value)}
-          options={sampleOptions}
-          label="フルーツを選んでね"
-          placeholder="選択してください"
-        />
-        <TitleInputField />
-        <CheckBox />
-        <SettingElement type="top" />
-        <SettingElement type="center" />
-        <SettingElement type="bottom" />
-        <SettingInfoIndex />
-        <LifeGaugeListElement />
-        <MonologueLine />
-        <MonologueLine type="talk" />
-      </Flex>
+      <MotionImage
+        src="/assets/svg/applogo.svg"
+        width="20%"
+        initial={{ scale: 1.04 }}
+        animate={{ scale: 1.0 }}
+        transition={{
+          ease: "easeInOut",
+        }}
+      />
     </Flex>
   );
 };
