92731f7c1283d8dfc25d64b9bb9a716a0403dcb0
Satsuki Hara <har06git13@gmail.com>
2026-02-22 15:29:53 +0900

    toggleのファイル名のエラーを直した

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -2,4 +2,4 @@ export { Button } from "./Button";
 export { Header } from "./Header";
 export { LifeGauge } from "./LifeGauge";
 export { LifeGaugeElement } from "./LifeGaugeElement";
-export { Toggle } from "./Toggle";
+export { Toggle } from "./toggle";
